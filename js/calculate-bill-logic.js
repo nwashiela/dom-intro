6e387f7365b41fd57f79fe51c7e040ff0ec47351
nwashiela <nwmahela12@gmail.com>
2020-06-29 16:15:41 +0200

    testing setup

---
+++ js/calculate-bill-logic.js
@@ -0,0 +1,20 @@
+
+function billTotal(billString){
+
+    var billItems = 
+        billString.split(",");
+    var billTotal = 0;
+for (var i=0; i<billItems.length; i++){
+
+var billItem = billItems[i].trim();
+if (billItem === "call"){
+billTotal += 2.75;
+}
+else if (billItem === "sms"){
+billTotal += 0.75;
+}
+
+}
+var roundedBillTotal = billTotal.toFixed(2);
+return roundedBillTotal; 
+}
