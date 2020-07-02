a81f0b36acbc7a9bf02234165c37916952517d99
nwashiela <nwmahela12@gmail.com>
2020-07-02 05:35:33 +0200

    calculate bill test done

--- test/calculate-bill.test.js
+++ test/calculate-bill.test.js
@@ -1,5 +1,11 @@
-describe("The bill with calculate factory" , function(){
-    asserts.equal(billString.addsmscall());
-
-})
-    
+  describe("calculateBill,take calls cost and sms cost return total cost",function(){
+    it("enter calls only and return total cost for calls captured",function(){
+      assert.equal(calculateBillEvent("call"), 2.75);
+    });
+    it("enter sms only and return total cost for sms captured",function(){
+      assert.equal(calculateBillEvent("sms"), 0.75);
+    });
+    it("enter sms and calls and return total cost",function(){
+      assert.equal(calculateBillEvent("sms,sms,call,call"), 7.00);
+    });
+  });
