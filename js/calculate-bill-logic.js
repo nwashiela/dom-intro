826d47a41cf5b0e163f498c269899d74b9cb99a0
nwashiela <nwmahela12@gmail.com>
2020-07-02 05:29:02 +0200

    dom code and logic code working separately

--- js/calculate-bill-logic.js
+++ js/calculate-bill-logic.js
@@ -1,49 +1,17 @@
-function countingCalculateBills(){
-    var billTotal = 0;
-    var sms  = 0;
-    var call = 0;
-
-    var warningone = 0;
-    var dangerone = 0;
-function getsms(){
-return sms;
-}
-function getcall(){
-return call;
-}
-function addsmscall(){
-billTotal = call + sms;
-return billTotal;
-}
-
-function billTotal(billString){
-    var billItems = 
-        billString.split(",");
-    
-for (var i=0; i<billItems.length; i++){
-var billItem = billItems[i].trim();
-if (billItem === "call"){
-billTotal += 2.75;
-}
-else if (billItem === "sms"){
-billTotal += 0.75;
-}
-}
-return(billTotal);
-}
-function TotalColor(roundedBillTotal){
-if(addsmscall() >= warningone && addsmscall() < dangerone){
-return "warning";
-}
-    else if(addsmscall() >= dangerone){
-        return "danger";
+function calculateBillEvent(billCaptured){
+    var billItem = billCaptured.split(",");
+     var billTotal = 0;
+    for (var i=0;i<billItem.length;i++){
+    var billCount = billItem[i].trim();
+  
+        if (billCount === "call"){
+            billTotal += 2.75;
+        }
+  
+        if (billCount === "sms"){
+            billTotal += 0.75;
+        }
     }
-}
-return{
-    addsmscall,
-    getcall,
-    getsms,
-    TotalColor,
-    billTotal,
-}
-}
+    return billTotal.toFixed(2);
+  }
+  
--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,55 +1,38 @@
-//get a reference to the calculate button
+document.addEventListener('DOMContentLoaded', function(){
 
-//get a reference to the billTotal element
-
-//get a reference to the billString
-
-//create the function that will be called when the calculate button is pressed
-//  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
-
-//link the function to a click event on the calculate button
-const calculateBtn = document.querySelector(".calculateBtn");
-const billTotalElement = document.querySelector(".billTotal");
-const billStringField = document.querySelector(".billString");
-const billTotalSpan = document.querySelector(".total");
-
-function billTotal(billString){
-
-var roundedBillTotal = billTotal.toFixed(2);
-return roundedBillTotal; 
-}
-
-function TotalColor(roundedBillTotal){
-
-billTotalSpan.classList.remove("danger");
-billTotalSpan.classList.remove("warning");
-
-const currentTotal = Number(roundedBillTotal);
-if (currentTotal >= 30){
-billTotalSpan.classList.add("danger");
-}
-else if(currentTotal >=20 && currentTotal <30){
-
-billTotalSpan.classList.add("warning");
-}
-
-}
-
-
-function calculateBtnClicked(){
-
-    var billString = billStringField.value;
-
-const roundedBillTotal = billTotal(billString);
-
-TotalColor(roundedBillTotal);
-
-billTotalElement.innerHTML = roundedBillTotal;
-
-}
-
-calculateBtn.addEventListener ("click",  calculateBtnClicked);
-
+    //get a reference to the calculate button
+    var calculateBtnElement = document.querySelector(".calculateBtn");
+    //get a reference to the billTotal element
+    var billTotalElement = document.querySelector(".billTotal");
+    //get a reference to the billString
+    var billStringElement = document.querySelector(".billString");
+    
+    // Dom function
+    function calculateBtnClicked(){
+    
+      // get the string entered in the text Area
+      var billString = billStringElement.value;
+      //call logic function
+      var roundedBillTotal = calculateBillEvent(billString);
+      billTotalElement.innerHTML = roundedBillTotal;
+    // change color to orange when amount is greater or equal to 20
+      if ( roundedBillTotal >= 20){
+        billTotalElement.classList.add("warning");
+      }
+    
+      if ( roundedBillTotal < 20){
+        billTotalElement.classList.remove("warning");
+      }
+    // change color to red when amount is greater or equal to 30
+          if(roundedBillTotal > 30  ){
+        billTotalElement.classList.add("danger");
+      }
+    
+      if(roundedBillTotal < 30  ){
+      billTotalElement.classList.remove("danger");
+    }
+    }
+    // add event listener for calculate button
+    calculateBtnElement.addEventListener('click', calculateBtnClicked);
+    });
+    
