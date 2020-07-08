ecfc5b1f75bb0fb932fceed1dfeab91111777d86
nwashiela <nwmahela12@gmail.com>
2020-07-08 10:45:14 +0200

    fixed testing radio bill

--- test/radio-bill.test.js
+++ test/radio-bill.test.js
@@ -2,14 +2,26 @@ describe("Radio_Button widget", function () {
 
     it("should return cost for one call", function () {
         var countRadiobutton = radiobuttons();
-        countRadiobutton.getsms();
+        countRadiobutton.logiccode("call");
         
-        assert.equal(countRadiobutton.getTotals(), 2.75);
+        assert.equal(countRadiobutton.getcall(), 2.75);
     })
     it("should return cost for one sms", function () {
         var countRadiobutton = radiobuttons();
-        countRadiobutton.getTotals("sms");
+        countRadiobutton.logiccode("sms");
 
         assert.equal(countRadiobutton.getsms(), 0.75);
     })
+    it("should return Totalcost for two smses and two calls", function () {
+        var countRadiobutton = radiobuttons();
+        countRadiobutton.logiccode("sms");
+        countRadiobutton.logiccode("sms");
+        countRadiobutton.logiccode("call");
+        countRadiobutton.logiccode("call");
+
+        assert.equal(countRadiobutton.getsms(), 1.50);
+        assert.equal(countRadiobutton.getcall(), 5.50);
+        assert.equal(countRadiobutton.getTotals(), 7.00);
+    })
+
 })
